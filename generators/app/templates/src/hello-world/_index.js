9344d257df68318eafc6854c80d2b4ee284032c9
Julian reyes Escrigas <julian.reyes.escrigas@gmail.com>
2017-03-10 10:13:02 -0500

    allow use ES7 syntax/features on mocha tests

--- generators/app/templates/src/hello-world/_index.js
+++ generators/app/templates/src/hello-world/_index.js
@@ -1,3 +1,9 @@
-export default function helloWorld() {
+export function helloWorld() {
   return 'Hello World';
 }
+
+export function helloWorldAsync() {
+  return Promise.resolve(`Async ${helloWorld()}`);
+}
+
+export default helloWorld;
--- generators/app/templates/test/hello-world/_index.test.js
+++ generators/app/templates/test/hello-world/_index.test.js
@@ -1,9 +1,12 @@
 import expect from 'expect.js';
-import helloWorld from '../../src/hello-world';
-
+import { helloWorld, helloWorldAsync } from '../../src/hello-world';
 
 describe('Hello World', () => {
   it('returns Hello World', () => {
     expect(helloWorld()).to.be('Hello World');
   });
+
+  it('returns async Hello world', async () => {
+    expect(await helloWorldAsync()).to.be('Async Hello World');
+  })
 });
